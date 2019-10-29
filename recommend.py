ba90a57660841ed768ef0c992d97caabc13f44e1
Anastasiia <ans.dudarenko@gmail.com>
2019-10-29 15:38:06 +0100

    Add ArgParser to recommendation.py

--- recommend.py
+++ recommend.py
@@ -1,4 +1,13 @@
 from SteamData import SteamData
+import argparse
+import sys
+
+ag = argparse.ArgumentParser()
+ag.add_argument('-g', '--game', type=str, required=False, help="Game title to predict recommendation to")
+ag.add_argument('-u', '--user', type=int, required=False, help="User id to predict recommendation to")
+ag.add_argument('-r_num', '--recommendation_number', required=False, type=int, help="Number of recommendation")
 
 recommend = SteamData()
 recommend.predict_games("Assassin's Creed")
+
+sys.stdout.flush()
